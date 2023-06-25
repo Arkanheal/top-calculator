05fbe8a165940f00f11331e07b984b084ffeb333
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-06-25 17:38:34 +0200

    Add core operations and html+css basics

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return "ERR"
+  return a / b;
+}
+
+function operate(firstNumber, secondNumber, operator){
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = add(firstNumber, secondNumber);
+      break;
+    case "-":
+      result = substract(firstNumber, secondNumber);
+      break;
+    case "*":
+      result = multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      result = divide(firstNumber, secondNumber);
+      break;
+    default:
+      result = "ERR Invalid Operator";
+      console.error(operator);
+      break;
+  }
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
