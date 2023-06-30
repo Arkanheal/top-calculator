4a0cf1e9fb48dc6cf91abcb5000ebf32614d47cd
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-06-30 20:53:13 +0200

    Add basic operations

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(firstNumber, secondNumber, operator){
+function operate(firstNumber, secondNumber, operator) {
   let result = 0;
   switch (operator) {
     case "+":
@@ -35,8 +35,43 @@ function operate(firstNumber, secondNumber, operator){
       console.error(operator);
       break;
   }
+  return result;
 }
 
 let firstNumber;
-let operator;
-let secondNumber;
+let operator = "";
+
+const screenDiv = document.querySelector(".screen");
+let screenDisplay = ""
+
+const numberElements = document.querySelectorAll(".number");
+numberElements.forEach(element => {
+  element.addEventListener("click", () => {
+    if (firstNumber && !operator) return;
+    let number = element.textContent;
+    screenDisplay == "0" ? screenDisplay = number : screenDisplay += number;
+    screenDiv.textContent = screenDisplay;
+  });
+});
+
+const operatorElements = document.querySelectorAll(".operator");
+operatorElements.forEach(element => {
+  element.addEventListener("click", () => {
+    if (operator || screenDisplay === "") return;
+    firstNumber = parseFloat(screenDisplay);
+    operator = element.textContent;
+    screenDisplay += ` ${operator} `;
+    screenDiv.textContent = screenDisplay;
+  });
+});
+
+const resultElement = document.querySelector(".result");
+resultElement.addEventListener("click", () => {
+  const displaySplit = screenDisplay.split(' ');
+  if (displaySplit.length !== 3) return;
+  const result = operate(firstNumber, parseFloat(displaySplit[2]), operator);
+  screenDisplay = result;
+  firstNumber = screenDisplay;
+  operator = "";
+  screenDiv.textContent = screenDisplay;
+});
